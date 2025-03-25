31b7c143015b76a25e4283900d5a89b0013c6fca
Ankit Kumar Yadav <ankityadav18193@gmail.com>
2025-03-25 10:33:57 +0000

    learning abiut maths and numbers

---
+++ 01-basics/07_Strings.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+let name = "ankit"
+let age = 22
+console.log(name + age); // this way becomes outdated in programing 
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+let userName = new String(`chotu`)
+console.log(userName.length);
+console.log(userName.indexOf('o'));
+let newUser = userName.substring(0, 4)
+console.log(newUser);
+let user = userName.slice(0, 3)
+console.log(user);
+
+let newUserOne="   ankit     "
+console.log(newUserOne);
+console.log(newUserOne.trim());
+let url="ankitKumarYadav@google.com"
+console.log(url.replace('Kumar','Kr'))
+ let info="currently Im pursuing BE in compurt science engineering"
+
+console.log(info.split(' '));
+
+
+
+
+
---
+++ 01-basics/08_NumsAndMaths.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let marks=100;
+let balance=new Number(2000)
+console.log(marks);
+console.log(balance);
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(3));
+console.log(balance.toPrecision(4));
+let hundreds=100000000000
+console.log(hundreds.toLocaleString('en-In'));
+
+
+
+//  ************* Maths **********
+console.log(Math.random());
+
+
+
