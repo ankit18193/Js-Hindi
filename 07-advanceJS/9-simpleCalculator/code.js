43ce546e8e063d45151ebc257504cf1bdef4be0c
Ankit Kumar Yadav <ankityadav18193@gmail.com>
2025-04-12 16:29:13 +0000

    promises

--- 07-advanceJS/9-simpleCalculator/code.js
+++
@@ -1,9 +0,0 @@
-const buttons=document.querySelector('#buttons')
-const display=document.querySelector('#display')
-
-
-let currentValue=""
-
-buttons.forEach((btn)=>{
-
-})
---
+++ 9-simpleCalculator/code.js
@@ -0,0 +1,11 @@
+const buttons=document.querySelector('.buttons')
+const display=document.querySelector('.display')
+
+
+let currentValue=""
+
+buttons.forEach((btn)=> {
+ btn.addEventListener('click',function(){
+    
+ })
+})
